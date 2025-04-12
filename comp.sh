08ca0bea1cb4a2ff90f5b4bd3dd8104513403bbf
homeserversample <homeserversample@gmail.com>
2025-04-12 20:35:57 +0000

    adding wrapper script for sudo docker compose

---
+++ comp.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#name for unique/temporary file to hold environment variables.
+ENVFILE=/tmp/$(uuidgen)
+
+# save PUID & PGID variables for linuxserver.io docker setup.
+cat > $ENVFILE << DONE
+PUID=$(id -u)
+PGID=$(id -g)
+DONE
+
+sudo docker compose --env-file $ENVFILE $@
